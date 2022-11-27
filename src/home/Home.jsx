11e9b36ac185ad33892be84260dfad46a8af8b35
zzinue <zzinue@gmail.com>
2022-11-27 10:41:52 -0600

    installing dependendices to frontend

---
+++ src/home/Home.jsx
@@ -0,0 +1,12 @@
+import { AcUnit } from "@material-ui/icons"
+import "./home.scss"
+
+const Home = () => {
+    return (
+        <div className='home'>
+            <AcUnit />
+        </div>
+    )
+}
+
+export default Home
